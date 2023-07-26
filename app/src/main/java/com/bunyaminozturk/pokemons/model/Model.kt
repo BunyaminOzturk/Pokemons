118691aca63e26ba73e3e6c5257263cbdffcce67
Bunyamin <bunyamin.ozturk.tr@gmail.com>
2023-07-26 14:06:58 +0300

    CardView and item_list done

---
+++ app/src/main/java/com/bunyaminozturk/pokemons/model/Model.kt
@@ -0,0 +1,4 @@
+package com.bunyaminozturk.pokemons.model
+
+class Model {
+}
---
+++ app/src/main/java/com/bunyaminozturk/pokemons/model/Pokedex.kt
@@ -0,0 +1,10 @@
+package com.bunyaminozturk.pokemons.model
+
+class Pokedex {
+    /*
+    public List<Pokemon> pokemon { get; set; }
+     */
+
+    var pokemon:List<Pokemon>? = null
+
+}
